8bbf6ebcb329ea6730708952313f89f8f29e0b2b
Jennifer A Miller <jennifer.a.miller2@gmail.com>
2014-10-01 15:29:49 -0400

    Remove Irrelevant Code from Skeleton.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -19,7 +19,7 @@ post '/login' do
 
     if @user.authenticate(params[:login][:password])
       session[:current_user_id] = @user.id
-      redirect to :"/user/#{@user.id}/dashboard"
+      redirect to :"/user/#{@user.id}"
     else
       session[:login_error] = "Username and/or password are incorrect" #QUESTION: Would it be desirable to add have my errors included in the same session hash as the activerecord errors (so all errors are in one place, and I could make a partial that referred to that one place, and use the partial on every page that required errors to be shown)? And should I use an error handler (error ... do ... end)?
       redirect to :'/login'
@@ -36,7 +36,7 @@ post '/signup' do
   @user = User.create(name: params[:signup][:name], password: params[:signup][:password])
   # binding.pry
   session[:current_user_id] = @user.id
-  redirect to :"/user/#{@user.id}/dashboard"
+  redirect to :"/user/#{@user.id}"
 end
 
 get '/logout' do
